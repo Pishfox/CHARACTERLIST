bff596187acafe4afe0da670050c6d22b92258d5
Pishfox <kwonnifox@gmail.com>
2019-12-01 15:24:37 +0700

    Update Build.java
    Successful compilation

--- WEB-INF/classes/com/corejsf/services/entities/Build.java
+++ WEB-INF/classes/com/corejsf/services/entities/Build.java
@@ -13,9 +13,9 @@ public class Build {
     доступна для записи, чтения, изменения и удаления*/
 
     private Long id;
-    private Characters character; //classes\com\corejsf\services\entities\Build.java:16: error: cannot find symbol class Characters
-    private Skills skill; //classes\com\corejsf\services\entities\Build.java:17: error: cannot find symbol class Skills
-    private Weapon weapon; //classes\com\corejsf\services\entities\Build.java:18: error: cannot find symbol class Weapon
+    private Characters character;
+    private Skills skill;
+    private Weapon weapon;
 
     public Long getId() {
         return id;
@@ -27,25 +27,25 @@ public class Build {
 
     public Characters getCharacters() {
 		return character;
-    } //classes\com\corejsf\services\entities\Build.java:28: error: cannot find symbol class Characters
+    }
 
     public void setCharacters(Characters character) {
         this.character = character;
-    } //classes\com\corejsf\services\entities\Build.java:32: error: cannot find symbol class Characters
+    }
 
     public Skills getSkills() {
         return skill;
-    } //classes\com\corejsf\services\entities\Build.java:36: error: cannot find symbol class Skills
+    }
 
     public void setSkills(Skills skill) {
         this.skill = skill;
-    } //classes\com\corejsf\services\entities\Build.java:40: error: cannot find symbol class Skills
+    }
 
     public Weapon getWeapon() {
         return weapon;
-    } //classes\com\corejsf\services\entities\Build.java:44: error: cannot find symbol class Weapon
+    }
 
     public void setWeapon(Weapon weapon) {
         this.weapon = weapon;
-    } //classes\com\corejsf\services\entities\Build.java:48: error: cannot find symbol class Weapon
+    }
 }
